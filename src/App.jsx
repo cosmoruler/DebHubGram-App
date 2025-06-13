445eed001aa5b1e17da0bef8f50457fbd3c172db
Kamaleswaran Krishna Kumar <39251381+cosmoruler@users.noreply.github.com>
2025-06-13 19:36:16 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
